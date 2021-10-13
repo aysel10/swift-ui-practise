bb8ea6749759631651427ee02c6c7a8edc7d749c
Aysel Abdullayeva <aysel.a@crocusoft.com>
2021-10-13 23:01:30 +0200

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by icelkas on 13/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
